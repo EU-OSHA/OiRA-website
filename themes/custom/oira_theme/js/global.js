84ddd5d0d515c1eb4c77cff8723ad0388223dd92
Ivan Arias <iarias@bilbomatica.es>
2021-10-25 13:18:42 +0200

    MDR-4641 - tab menu in mobile view not working

--- themes/custom/oira_theme/js/global.js
+++ themes/custom/oira_theme/js/global.js
@@ -167,9 +167,11 @@ jQuery(document).ready(function($){
     });
 
     /*** Show clear button on active facet ***/
-    let searchApiVal = $('#edit-search-api-fulltext--2').val();
-    if ( searchApiVal.length>0 || $(".block-facets").hasClass("facet-active") ) {
-      $("#edit-search-api-fulltext--2, .block-facets").closest(".section").find('#edit-reset--2').addClass("show-me");
+    if ($("#edit-search-api-fulltext--2")[0]) {
+      let searchApiVal = $('#edit-search-api-fulltext--2').val();
+      if (searchApiVal.length > 0 || $(".block-facets").hasClass("facet-active")) {
+        $("#edit-search-api-fulltext--2, .block-facets").closest(".section").find('#edit-reset--2').addClass("show-me");
+      }
     }
 
     //Move the summary before the menu (in node 67)
